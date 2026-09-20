d878fe4c5e2ad706bb334331f076b6165384dd6e
matheuspereiradev <matheuslima20111997@gmail.com>
2026-09-20 20:01:11 -0300

    add

---
+++ Service.Domain/Report/Results/BufferColorSummaryRow.cs
@@ -0,0 +1,10 @@
+using Service.Domain.Enums;
+
+namespace Service.Domain.Report.Results
+{
+    public class BufferColorSummaryRow
+    {
+        public BufferColor Color { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Service.Domain/Report/Results/InventoryBufferManagementColorSummaryResult.cs
@@ -0,0 +1,8 @@
+namespace Service.Domain.Report.Results
+{
+    public class InventoryBufferManagementColorSummaryResult
+    {
+        public List<BufferColorSummaryRow> Netflow { get; set; } = new();
+        public List<BufferColorSummaryRow> Execution { get; set; } = new();
+    }
+}
